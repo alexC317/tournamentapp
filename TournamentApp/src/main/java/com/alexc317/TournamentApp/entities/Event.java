2cd36db73c85f9dc534b8d6a1b10e452bbba7ad7
alexC317 <alex.f.cepeda@gmail.com>
2019-06-06 19:09:56 -0400

    First commit for this project! Created an application for tournament management and organization via Maven. Created classes for a Player, a Tournament and an Event.

---
+++ TournamentApp/src/main/java/com/alexc317/TournamentApp/entities/Event.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alexc317.TournamentApp.entities;
+
+/**
+ *
+ * @author Alex
+ */
+public class Event {
+    
+}
---
+++ TournamentApp/src/test/java/com/alexc317/TournamentApp/TournamentAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexc317.TournamentApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TournamentAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
