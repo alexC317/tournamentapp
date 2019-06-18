fbe1397416235b590b1747e68884304f71c3c35f
alexC317 <alex.f.cepeda@gmail.com>
2019-06-18 17:05:54 -0400

    Added getters and setters for the Game object's attributes.

--- TournamentApp/src/main/java/com/alexc317/TournamentApp/entities/Game.java
+++ TournamentApp/src/main/java/com/alexc317/TournamentApp/entities/Game.java
@@ -5,13 +5,60 @@
  */
 package com.alexc317.TournamentApp.entities;
 
+import java.util.Objects;
+
 /**
  *
  * @author Alex
  */
 public class Game {
-    
+
     private int ID;
     private String gameName;
-    
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + this.ID;
+        hash = 53 * hash + Objects.hashCode(this.gameName);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.ID != other.ID) {
+            return false;
+        }
+        if (!Objects.equals(this.gameName, other.gameName)) {
+            return false;
+        }
+        return true;
+    }
+
 }
