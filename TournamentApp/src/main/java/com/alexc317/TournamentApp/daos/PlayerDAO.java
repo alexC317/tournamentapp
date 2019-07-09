f6547fa1233b154882debf52bc23078050fad900
alexC317 <alex.f.cepeda@gmail.com>
2019-07-08 23:10:19 -0400

    Created PlayerDAO interface and JDBC implementation.

---
+++ TournamentApp/src/main/java/com/alexc317/TournamentApp/daos/PlayerDAO.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alexc317.TournamentApp.daos;
+
+import com.alexc317.TournamentApp.entities.Player;
+import java.util.List;
+
+/**
+ *
+ * @author Alex
+ */
+public interface PlayerDAO {
+
+    public Player addPlayer(Player player);
+
+    public List<Player> getAllPlayers();
+
+    public Player getPlayer(int ID);
+
+    public void editPlayer(int ID);
+
+    public void deletePlayer(int ID);
+}
---
+++ TournamentApp/src/main/java/com/alexc317/TournamentApp/daos/PlayerDAOImpl.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.alexc317.TournamentApp.daos;
+
+import com.alexc317.TournamentApp.entities.Player;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+
+public class PlayerDAOImpl implements PlayerDAO {
+
+    @Autowired
+    private JdbcTemplate jdbc;
+
+    private final String INSERT_NEW_PLAYER = "INSERT INTO Player(firstName, lastName, gamertag) VALUES (?, ?, ?)";
+    private final String SELECT_ALL_PLAYERS = "SELECT firstName, lastName, gamertag FROM Player";
+    private final String SELECT_PLAYER_BY_ID = "SELECT firstName, lastName, gamertag FROM Player WHERE ID = ?";
+    private final String EDIT_PLAYER = "UPDATE Player SET firstName = ?, lastName = ?, gamertag = ? WHERE ID = ?";
+    private final String DELETE_PLAYER = "DELETE FROM Player WHERE ID = ?";
+
+    @Override
+    public Player addPlayer(Player player) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Player> getAllPlayers() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Player getPlayer(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void editPlayer(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void deletePlayer(int ID) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
